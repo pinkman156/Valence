51fef522f56894269ed5fce8e6c9aeeacac4e76f
Nipun Kohli <nipunk75@gmail.com>
2021-06-05 03:17:10 +0530

    added redirect uri

--- src/components/config.js
+++ src/components/config.js
@@ -2,7 +2,7 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = "51d00219190a49e0bfc56014f5acc6d0";
-export const redirectUri = "http://localhost:3000/redirect";
+export const redirectUri = "https://valence-zeta.vercel.app/redirect";
 export const scopes = [
   "user-top-read",
   "user-read-currently-playing",
